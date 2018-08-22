ca2932c0fe76c1603c27683adfc9cf5371b4c440
safordog <40581256+safordog@users.noreply.github.com>
2018-08-22 14:21:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author safordog
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        File english = new File("English.in"); 
+        HashMap<String, String> vocab = new HashMap<>();
+        setVocabulary("i", "я", vocab);
+        setVocabulary("have", "маю", vocab);
+        setVocabulary("dream", "мрія", vocab);
+        setVocabulary("that", "що", vocab);
+        setVocabulary("one", "один", vocab);
+        setVocabulary("day", "день", vocab);
+        setVocabulary("this", "ця", vocab);
+        setVocabulary("nation", "нація", vocab);
+        setVocabulary("will", "буде", vocab);
+        setVocabulary("rise", "зростати", vocab);
+        setVocabulary("up", "вгору", vocab);
+        setVocabulary("and", "і", vocab);
+        setVocabulary("liveout", "усвідомити", vocab);
+        setVocabulary("true", "справжній", vocab);
+        setVocabulary("meaning", "зміст", vocab);
+        setVocabulary("its", "свого", vocab);
+        setVocabulary("creed", "віровчення", vocab);
+        System.out.println(getTranslate(vocab).toString());
+    }
+  
+    public static void setVocabulary(String eng, String ukr, HashMap<String, String> map) {
+        map.put(eng, ukr);
+        File file = new File("Vocabulary.txt");
+        try (PrintWriter pw = new PrintWriter(file)) {
+            map.entrySet().forEach((temp) -> {
+                pw.println(temp.getKey() + " - " + temp.getValue());
+            });
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+            }
+    }
+    
+    public static ArrayList getTranslate(HashMap map) throws IOException {
+        ArrayList<String> in = Files.lines(Paths.get("English.in"))
+                .collect(Collectors.toCollection(ArrayList::new));
+        String strIn = in.toString();
+        String[] arrStr = strIn.split(" ");
+        String strOut = "";
+        Path out = Paths.get("Ukrainian.out");
+        for (int i = 0; i < arrStr.length; i++) {
+            if (map.containsKey(arrStr[i])) {
+                strOut = strOut + (map.get(arrStr[i]) + " ");
+            }
+        }
+        ArrayList<String> arrOut = new ArrayList<>(Arrays.asList (strOut));
+        Files.write(out,arrOut);
+        return arrOut;
+    }
+    
+}
